a1a8ab52d64567f69a14822cfabc2ed0c8eefd77
Ghezzi Rayhan <rayhan.ghezzi@gmail.com>
2018-10-17 10:48:13 +0200

    Push fichier.txt ofstream fonctionnel ! Push tableau à taille modifiable selon le nombre de reel que souhaite l'utilisateur !

--- src/main.cpp
+++ src/main.cpp
@@ -14,28 +14,57 @@ int main(){
 
 string yesno;
 int inc;
+int n;
 
-vector <float> reel(10);
+vector <float> reel(inc);
 
 	cout<<"Vous disposez d'un tableau de réel auxquel vous pouvez y ajouter des nombres"<<endl;
 	cout<<"Voulez vous ajouter un nombre à votre tableau ?"<<endl;
 	cin>>yesno;
-	cout<<"Combien de nombre voulez vous entrer ?"<<endl;
-	cin>>inc;
 
-	ofstream save_tab;
-	save_tab.open("tableau.txt");
-	for(int i=0; i<inc; i++){
-		cout<<"Tapez le(s) nombre(s) que vous souhaitez entrer : "<<endl;
-		cin>>reel[i];
-		save_tab<<"Nombre stocké : "<<reel[i]<<endl;
+	if(yesno == "oui"){
+		cout<<"Combien de nombre voulez vous entrer ?"<<endl;
+		cin>>inc;
+
+		ofstream save_tab;
+		save_tab.open("tableau.txt");
+		for(int i=0; i<inc; i++){
+			cout<<"Tapez le(s) nombre(s) que vous souhaitez entrer : "<<endl;
+			cin>>reel[i];
+			save_tab<<"Nombre stocké : "<<reel[i]<<endl;
+		}
+		save_tab.close();
 	}
-	save_tab.close();
+	else {
+		cout<<"Fin du code"<<endl;
+		return 0;
+	}
 
 	cout<<"Voulez afficher les reel stockés dans le tableau ?"<<endl;
 	cin>>yesno;
-	for(int i=0; i<inc; i++){
-		cout<<"Nombre stocké : "<<reel[i]<<endl;
+
+	if(yesno == "oui"){
+		for(int i=0; i<inc; i++){
+			cout<<"Nombre stocké : "<<reel[i]<<endl;
+		}
 	}
+	else {
+		cout<<"Fin du code"<<endl;
+		return 0;
+	}
+	cout<<"Voulez supprimer le premier réel du tableau ? "<<endl;
+	cin>>yesno;
+
+	if(yesno == "oui"){
+		cout<<"Voici ce qu'il reste du tableau :"<<endl;
+			for(int i=0; i<reel.size(); i++){
+				cout<<"Nombre restant : "<<reel[i]<<endl;
+			}
+	}
+	else{
+		cout<<"Fin du programme"<<endl;
+		return 0;
+	}
+
 	return 0;
 }
