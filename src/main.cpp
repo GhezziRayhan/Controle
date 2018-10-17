184a4e672e7432f4c05e1c5e299532f7ce044c27
Ghezzi Rayhan <rayhan.ghezzi@gmail.com>
2018-10-17 02:38:20 +0200

    Push tableau avec float fonctionnel ! (youpi)

--- src/main.cpp
+++ src/main.cpp
@@ -12,28 +12,30 @@ using namespace std;
 
 int main(){
 
-string nombre;
 string yesno;
 int inc;
 
-vector <float> reel;
-vector <int> number;
+vector <float> reel(10);
 
 	cout<<"Vous disposez d'un tableau de réel auxquel vous pouvez y ajouter des nombres"<<endl;
 	cout<<"Voulez vous ajouter un nombre à votre tableau ?"<<endl;
 	cin>>yesno;
-	cout<<"Combien d'entier voulez vous entrer ?"<<endl;
+	cout<<"Combien de nombre voulez vous entrer ?"<<endl;
 	cin>>inc;
 
+	ofstream save_tab;
+	save_tab.open("tableau.txt");
 	for(int i=0; i<inc; i++){
-	cout<<"Tapez le(s) nombre(s) que vous souhaitez entrer : "<<endl;
-	cin>>nombre[i];
+		cout<<"Tapez le(s) nombre(s) que vous souhaitez entrer : "<<endl;
+		cin>>reel[i];
+		save_tab<<"Nombre stocké : "<<reel[i]<<endl;
 	}
+	save_tab.close();
 
 	cout<<"Voulez afficher les reel stockés dans le tableau ?"<<endl;
 	cin>>yesno;
 	for(int i=0; i<inc; i++){
-		cout<<"Nombre stocké : "<<nombre[i]<<endl;
+		cout<<"Nombre stocké : "<<reel[i]<<endl;
 	}
 	return 0;
 }
