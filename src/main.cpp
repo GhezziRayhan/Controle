d72022fe2b0e6ccfa571647e2e46319e2d03098f
Ghezzi Rayhan <rayhan.ghezzi@gmail.com>
2018-10-17 12:56:45 +0200

    12h56 Push de la documentation doxygen et mise à jour du code

--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,32 @@ using namespace std;
 
 string choix;
 int inc;
-int suppr;
-int n;
 int i;
+int suppr;
+
+vector <float> reel(3);
+
+/*! \ Main du fichier !
+* \ Le main contient un menu qui permet plusieurs commandes : Ajouter des nombres à virgules , Afficher les nombres stocker, Supprimer un nombre donné ou plusieusr nombre donnés (non fonctionnel) ainsi que quitter le menu. Le main.cpp crée également un fichier.txt où on stock les nombres ajoutés.
+* \ Le main retourne les nombres lorsqu'on lui demande.
+*/
 
 int main() {
-vector <float> reel(10);
-
 	cout<<"Vous disposez d'un tableau de réel auquel vous pouvez y ajouter des nombres"<<endl;
+
+/*! \ While
+* \ Il permet d'excuter le menu en continue et met fin au programme si on tape la commande EXIT
+*/
+
 	while(choix != "EXIT"){
 		cout<<"Que voulez faire ?"<<endl;
-		cout<<"Commandes : AJOUTER / AFFICHER / SUPPRIMER / SUPPR / EXIT"<<endl;
+		cout<<"Commandes : AJOUTER / AFFICHER / SUPPR / SUPPRIMER / EXIT"<<endl;
 		cin>>choix;
 		cout<<" "<<endl;
+
+/*! \ Commande Ajouter
+* \ Permet de d'ajouter des réels dans le tableaux reel.
+*/
 
 		if(choix == "AJOUTER"){
 
@@ -43,17 +56,37 @@ vector <float> reel(10);
 			}
 			save_tab.close();
 		}
+
+/*! \ Commande Afficher
+* \ Permet d'afficher les chiffres ajouter dans le tableau
+*/
 		if(choix == "AFFICHER"){
 			for(i=0; i<inc; i++){
 				cout<<"Nombre stocké : "<<reel[i]<<endl;
 				cout<<" "<<endl;
 			}
 		}
+
+/*! \ Commande Suprr
+* \ Est censé supprimer un réel donnée (ne marche pas)
+*/
 		if(choix == "SUPPR"){
-			for(i=0; i<reel.size(); i++){
-			reel[i].erase;
+			cout<<"Quel nombre supprimer ?"<<endl;
+			cin>>suppr;
+			for(i=0; i++;){
+				if(i==suppr){
+					cout<<"coucou"<<endl;
+				}
 			}
+		}
+
+/*! \ Commande Supprimer
+* \ Est censé supprimer tout le tableau (ne marche pas)
+*/
+		if(choix == "SUPPRIMER"){
+			reel.clear();
 		}
 	}
 		return 0;
+
 }
