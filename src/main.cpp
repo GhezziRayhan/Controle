0c4bfb53f6bdef7844d3470e0eab379b9189b93a
Ghezzi Rayhan <rayhan.ghezzi@gmail.com>
2018-10-17 12:19:17 +0200

    Push tableau float avec menu, seul AJOUTER / AFFICHER marchent

--- src/main.cpp
+++ src/main.cpp
@@ -10,61 +10,50 @@
 #include <fstream>
 using namespace std;
 
-int main(){
-
-string yesno;
+string choix;
 int inc;
+int suppr;
 int n;
+int i;
 
-vector <float> reel(inc);
+int main() {
+vector <float> reel(10);
 
-	cout<<"Vous disposez d'un tableau de réel auxquel vous pouvez y ajouter des nombres"<<endl;
-	cout<<"Voulez vous ajouter un nombre à votre tableau ?"<<endl;
-	cin>>yesno;
+	cout<<"Vous disposez d'un tableau de réel auquel vous pouvez y ajouter des nombres"<<endl;
+	while(choix != "EXIT"){
+		cout<<"Que voulez faire ?"<<endl;
+		cout<<"Commandes : AJOUTER / AFFICHER / SUPPRIMER / SUPPR / EXIT"<<endl;
+		cin>>choix;
+		cout<<" "<<endl;
 
-	if(yesno == "oui"){
-		cout<<"Combien de nombre voulez vous entrer ?"<<endl;
-		cin>>inc;
+		if(choix == "AJOUTER"){
 
-		ofstream save_tab;
-		save_tab.open("tableau.txt");
-		for(int i=0; i<inc; i++){
-			cout<<"Tapez le(s) nombre(s) que vous souhaitez entrer : "<<endl;
-			cin>>reel[i];
-			save_tab<<"Nombre stocké : "<<reel[i]<<endl;
-		}
-		save_tab.close();
-	}
-	else {
-		cout<<"Fin du code"<<endl;
-		return 0;
-	}
+			cout<<"Combien de nombre voulez vous entrer ?"<<endl;
+			cin>>inc;
+			cout<<" "<<endl;
 
-	cout<<"Voulez afficher les reel stockés dans le tableau ?"<<endl;
-	cin>>yesno;
+			ofstream save_tab;
+			save_tab.open("tableau.txt");
 
-	if(yesno == "oui"){
-		for(int i=0; i<inc; i++){
-			cout<<"Nombre stocké : "<<reel[i]<<endl;
-		}
-	}
-	else {
-		cout<<"Fin du code"<<endl;
-		return 0;
-	}
-	cout<<"Voulez supprimer le premier réel du tableau ? "<<endl;
-	cin>>yesno;
-
-	if(yesno == "oui"){
-		cout<<"Voici ce qu'il reste du tableau :"<<endl;
-			for(int i=0; i<reel.size(); i++){
-				cout<<"Nombre restant : "<<reel[i]<<endl;
+			for(i=0; i<inc; i++){
+				cout<<"Tapez le(s) nombre(s) que vous souhaitez entrer : "<<endl;
+				cin>>reel[i];
+				save_tab<<"Nombre stocké : "<<reel[0+i]<<endl;
+				cout<<" "<<endl;
 			}
+			save_tab.close();
+		}
+		if(choix == "AFFICHER"){
+			for(i=0; i<inc; i++){
+				cout<<"Nombre stocké : "<<reel[i]<<endl;
+				cout<<" "<<endl;
+			}
+		}
+		if(choix == "SUPPR"){
+			for(i=0; i<reel.size(); i++){
+			reel[i].erase;
+			}
+		}
 	}
-	else{
-		cout<<"Fin du programme"<<endl;
 		return 0;
-	}
-
-	return 0;
 }
